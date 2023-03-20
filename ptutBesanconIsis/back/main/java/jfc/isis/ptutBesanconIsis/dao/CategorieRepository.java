007671e46d8136c9ffe4828ac8f7bac09135c6a1
Laurine2905 <laurine.rat@outlook.fr>
2023-03-20 10:28:23 +0100

    maven fait des siennes mais si on regle ca il y a moyen que ca fonctionne

---
+++ ptutBesanconIsis/back/main/java/jfc/isis/ptutBesanconIsis/dao/CategorieRepository.java
@@ -0,0 +1,10 @@
+package jfc.isis.ptutBesanconIsis.dao;
+
+
+import jfc.isis.ptutBesanconIsis.entity.Categorie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
+
+}
