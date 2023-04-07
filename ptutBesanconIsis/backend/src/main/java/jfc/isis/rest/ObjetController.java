9804301f58796d59c496466f9bff77e29a53b5e3
amandine3110 <amandine.villatte@etud.univ-jfc.fr>
2023-04-07 15:06:20 +0200

    commit creation objetService

---
+++ ptutBesanconIsis/backend/src/main/java/jfc/isis/rest/ObjetController.java
@@ -0,0 +1,34 @@
+package jfc.isis.rest;
+
+import jfc.isis.dao.CategorieRepository;
+import jfc.isis.dao.ObjetRepository;
+import jfc.isis.service.CategorieService;
+import jfc.isis.service.ObjetService;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/service/objet")
+public class ObjetController {
+
+
+    private final ObjetService objetService;
+    private final ModelMapper mapper;
+
+    public ObjetController(ObjetService objetService, ModelMapper mapper) {
+        this.objetService = objetService;
+        this.mapper = mapper;
+    }
+
+    @PostMapping("objetsParCategorie")
+    public ObjetRepository objetsParCategorie(@RequestParam int idDeLaCategorie){
+        var objets = objetService.getObjetsParCategorie(idDeLaCategorie);
+        return mapper.map(objets,ObjetRepository.class);
+    }
+    //Exemple : http://localhost:8989/service/objet/objetsParCategorie?idDeLaCategorie=1
+
+
+}
