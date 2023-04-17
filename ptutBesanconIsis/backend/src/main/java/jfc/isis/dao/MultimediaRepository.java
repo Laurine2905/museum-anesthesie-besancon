47ec62c7d1d9a33295dc9a9819d0b5c005949918
Laurine2905 <laurine.rat@outlook.fr>
2023-04-17 21:00:01 +0200

    update

--- ptutBesanconIsis/backend/src/main/java/jfc/isis/dao/MultimediaRepository.java
+++
@@ -1,8 +0,0 @@
-package jfc.isis.dao;
-
-import jfc.isis.entity.Multimedia;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MultimediaRepository extends JpaRepository<Multimedia, Integer> {
-
-}
